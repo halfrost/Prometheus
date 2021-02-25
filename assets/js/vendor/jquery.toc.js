b275ed2bfd333db01ab317c17bed89d7f34745ac
strongduanmu <strongduanmu@gmail.com>
2021-02-25 21:32:59 +0800

    fix toc href duplication when title starts with same word

--- assets/js/vendor/jquery.toc.js
+++ assets/js/vendor/jquery.toc.js
@@ -154,7 +154,7 @@
         var defaultOpts = {
             selector: 'h1, h2, h3, h4, h5, h6',
             scope: 'body',
-            overwrite: false,
+            overwrite: true,
             prefix: 'toc'
         };
 
